90fb4226ea87d2e7dc60232385dacdb06900ab69
Yanat Mehdi <78317632+Mehdi-Yanat@users.noreply.github.com>
2022-05-18 16:44:31 +0100

    connect to db via localhost

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/hablamos', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
+mongoose.connect('mongodb://127.0.0.1:27017/hablamos', {
+     useNewUrlParser: true,
+     useCreateIndex: true,
+     useUnifiedTopology: true,
+     useFindAndModify: false
+ });
 
 //OLD CLOUD DB
 // mongoose.connect('mongodb+srv://nasykh:5fXrr7Yptvdcokyu@cluster0.wqtyq.mongodb.net/hablamos?retryWrites=true&w=majority', {
@@ -16,9 +16,9 @@ const mongoose = require('mongoose');
 // });
 
 //NEW CLOUD DB
-mongoose.connect('mongodb+srv://nasro_ykh:ASl2T1Esii9NNwcs@cluster0.tz0rx.mongodb.net/hablamos?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
+//mongoose.connect('mongodb+srv://nasro_ykh:ASl2T1Esii9NNwcs@cluster0.tz0rx.mongodb.net/hablamos?retryWrites=true&w=majority', {
+  //  useNewUrlParser: true,
+   // useCreateIndex: true,
+   // useUnifiedTopology: true,
+   // useFindAndModify: false
+//});
